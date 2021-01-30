e7fdad53e5c1511fc0e36d960b158de399f425e6
Theodore Jensen <30739356+Tedtanium@users.noreply.github.com>
2021-01-29 19:13:13 -0800

    Add playerCount module (mostly complete)

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 
 # Modules:
 ## playerCount Module
+def playerCount():
+  rePattern = re.compile('(joined|left) this ARK!')
+  joinLeft = rePattern.findall(p.read_text(.//logs//ShooterGame.log))
+  playerJoined = joinLeft.count('joined')
+  playerLeft = joinLeft.count('left')
+  playerCount = playerJoined - playerLeft
+  # Recurrence/timers?
+
 ## serverStatus Checker Module
 ## serverHasBeenEmpty Checker Module
 ## Server Starter Module
