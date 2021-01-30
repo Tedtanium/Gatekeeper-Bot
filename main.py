585ca419277ae407cb580fb7130c964aa999164c
Theodore Jensen <30739356+Tedtanium@users.noreply.github.com>
2021-01-29 18:59:49 -0800

    Add skeleton of skeleton template

---
+++ main.py
@@ -0,0 +1,14 @@
+# import lines
+
+
+# Modules:
+## playerCount Module
+## serverStatus Checker Module
+## serverHasBeenEmpty Checker Module
+## Server Starter Module
+## Server Terminator Module
+## After-Hours Shutdown Module
+
+# Discord-related Modules:
+## Mood Updater Module
+## Command Receiver Module
