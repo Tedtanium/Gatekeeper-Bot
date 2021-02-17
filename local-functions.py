37fd86857ef1f5840e91ee319ecf4439e6dc39cc
Theodore Jensen <30739356+Tedtanium@users.noreply.github.com>
2021-02-17 10:17:19 -0800

    Rename main.py, prep to hook up to management.py

--- local-functions.py
+++ local-functions.py
@@ -2,46 +2,20 @@
 import time
 import datetime
 import os
-import discord
 import psutil
 from rcon import Client
 
-# Init
-
-# A place to dump variables that may change easily in the future.
-class var_dump():
-    ipaddr = '0.0.0.0'
-    port = 0000
-    passwd = '0000'
+# A place to dump variables that may change easily in the future or should be initialized with 0.
+class local_var_dump():
     inactivity_time = 0
-    start_hour = datetime.time(6)
-    end_hour = datetime.time(22)
       
-    # init method/constructor 
-def __init__(self): 
-    self.sec = 0 
-    self.inactivity_time = 0
-                       
-          
-    # time check function every 30s
-def tick_check(self): 
-    time.sleep(1)
-    self.sec += 1
-    if self.sec % 30 == 0 and "ShooterGameServer.exe" in (p.name() for p in psutil.process_iter()) == True:
-      server_status, players_online = server_status_check()
-      server_inactivity_checker(players_online)
-    if self.sec % 600 == 0 and "ShooterGameServer.exe" in (p.name() for p in psutil.process_iter()) == False:
-      after_hours_shutdown()
-       
-  
 
-  
 
 ## playerCount function
-def server_status_check():
+def server_status_check(ipaddr, port, passwod):
   # Instigates connection to RCON and tries to get a playercount; if this fails the server is still starting.
     try:
-        with Client(var_dump.ipaddr, var_dump.port, passwd=var_dump.passwd) as client:
+        with Client(ipaddr, port, passwd=passwod) as client:
             players_online = client.run('listplayers')
         server_status = 'Up'
         if 'No Players Connected' in players_online:
@@ -72,8 +46,8 @@ def server_inactivity_checker(players_online):
 ## Server Terminator function
 
 ## After-Hours Shutdown function - This is running on the assumption that it's a 24h clock and not a 12h, which is untested at this time.
-def after_hours_shutdown():
-    if var_dump.end_hour <= datetime.datetime.now().time() <= var_dump.start_hour:
+def after_hours_shutdown(start_hour, end_hour):
+    if end_hour <= datetime.datetime.now().time() <= start_hour:
         os.system("shutdown /s /t 1")
     else:
         return('The time is ' + str(datetime.datetime.now().time()) + '. It\'s not time to shut down yet!')
