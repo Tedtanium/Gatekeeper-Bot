e6bc652edde1bc8f4ea5c09567577870a94d7f51
Theodore Jensen <30739356+Tedtanium@users.noreply.github.com>
2021-02-19 13:47:27 -0800

    Create moodUpdater.py

---
+++ cogs/moodUpdater.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands, tasks
+import os
+import psutil
+import asyncio
+
+
+client = commands.Bot(command_prefix = '>')
+
+
+class moodUpdater(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        #local variables needed initialized here.
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('moodUpdater cog loaded.')
+
+    async def statusUpdate(self, ctx, serverStatus):
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Server | Status: " + serverStatus))
+
+
+def setup(bot):
+    bot.add_cog(moodUpdater(bot))
