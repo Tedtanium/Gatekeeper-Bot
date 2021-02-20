415689eedfee0e6cf8774889bc8565bc3be1446b
Theodore Jensen <30739356+Tedtanium@users.noreply.github.com>
2021-02-20 14:59:19 -0800

    Update core.py

--- core.py
+++ core.py
@@ -7,7 +7,7 @@ import datetime
 import psutil
 #Server Status Check Only
 from rcon import Client
-import gatekeeper.management
+import gatekeeper.management as mgmt
 
 
 
@@ -33,40 +33,42 @@ class Core(commands.Cog):
         self.endHour = datetime.time(22)
         self.core.start()
         
-        
 ####### Remains of TickCheck - Not sure what to do/where to put this code at the moment. ######################
     @tasks.loop(seconds=1.0)
-    async def core:
+    async def core(self):
         ### Runs server_status_check if server is up every 30 seconds. ######
-        if self.bot.get_cog('Ticker').timePassed % 30 == 0:
-            serverTest = management.serverTest
+        if self.bot.get_cog('Ticker').timePassed % 5 == 0:
+            serverTest = await mgmt.Management.serverTest(self)
             if serverTest == True:
-                self.serverStatus, self.playerCount = await management.serverStatusCheck(self.ipaddr, self.port, self.passwod)
+                self.serverStatus, self.playerCount = await mgmt.Management.serverStatusCheck(self.ipaddr, self.port, self.passwod)
                 print('Server Status Check: The server is currently ' + self.serverStatus + ' with ' + str(self.playerCount) + ' players online.')
                 # Updates the bot's status with the current server status.
                 await self.bot.get_cog('MoodUpdater').statusUpdate(self, self.serverStatus)
                 self.bot.get_cog('ServerActions').serverStatus = self.serverStatus
                 # Runs inactivityChecker cog.
-                await management.inactivityChecker(self.playerCount)
+                await mgmt.Management.inactivityChecker(self.playerCount)
             else:
                 print('Server status check: Server is offline!')
                 await self.bot.get_cog('MoodUpdater').statusUpdate(self, 'Down')
         ### Runs after_hours_shutdown if server is not up, every 5 minutes. ######
         if self.bot.get_cog('Ticker').timePassed % 600 == 0:
-            serverTest = management.serverTest()
+            serverTest = await mgmt.Management.serverTest(self)
             if serverTest == False:
-                await management.afterHoursShutdown(self.startHour, self.endHour)
+                await mgmt.afterHoursShutdown(self.startHour, self.endHour)
             else:
                 print('After-Hours Shutdown: Server is still running!')
 
 #############################################################################################################
 
+    @core.before_loop
+    async def before_core(self):
+        print('Waiting...')
+        await self.bot.wait_until_ready()
 
-
+client.add_cog(Core(client))
 
 ######### Loads all cogs in cogs subfolder so long as they end in .py, cutting off the file extension. ######
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 client.run(bot_token)
-
