b4b0ff8f9e48dcfcbc06c3cab8c4a380c5a20cea
Theodore Jensen <30739356+Tedtanium@users.noreply.github.com>
2021-02-20 11:08:51 -0800

    Update ticker.py

--- cogs/ticker.py
+++ cogs/ticker.py
@@ -10,14 +10,26 @@ client = commands.Bot(command_prefix = '>')
 class Ticker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        #local variables needed initialized here.
+        self.timePassed = 0
+        self.tickCheck.start()
+        
+        
+    @tasks.loop(seconds=1.0)
+    async def tickCheck(self):
+        self.timePassed += 1
 
+    def cog_unload(self):
+        self.tickCheck.cancel()
+        
 
     @commands.Cog.listener()
     async def on_ready(self):
         print('Ticker cog loaded.')
 
-
+    @tickCheck.before_loop
+    async def before_tick(self):
+        print('Waiting...')
+        await self.bot.wait_until_ready()
 
 def setup(bot):
     bot.add_cog(Ticker(bot))
