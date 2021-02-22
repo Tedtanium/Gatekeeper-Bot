2ff76ff88e9c83ec06db60523da92cace034f6c8
Theodore Jensen <30739356+Tedtanium@users.noreply.github.com>
2021-02-22 13:21:39 -0800

    Possible final rewrite

--- gatekeeper/management.py
+++ gatekeeper/management.py
@@ -7,87 +7,109 @@ import datetime
 import psutil
 from rcon import Client
 
-class Management():
-    def __init__(self):
-        self.serverStatus = 'Down'
+serverStatus = 'Down'
+ipaddr = '127.0.0.1'
+port = 27020
+passwod = '_yeetles_'
+canSave = True
         
 ###################### Start Server #####################
 
-    async def bootServer(self, filepath):
-        os.startfile(filepath)
+async def bootServer(self, filepath):
+    os.startfile(filepath)
         
 #########################################################
 
 ###################### Stop Server ######################
 
-    async def terminateServer():
-        target = 'ShooterGameServer.exe'
-        os.system('TASKKILL /IM ' + target)
+async def terminateServer():
+    target = 'ShooterGameServer.exe'
+    os.system('TASKKILL /IM ' + target)
 
 #########################################################
 
 ##############  Server exe is Running Test ##############
 
-    async def serverTest(self):
-        serverProcessExists = "ShooterGameServer.exe" in (p.name() for p in psutil.process_iter())
-        return(serverProcessExists)
+async def serverTest(self):
+    serverProcessExists = "ShooterGameServer.exe" in (p.name() for p in psutil.process_iter())
+    return(serverProcessExists)
     
 #########################################################
 
 ###################### Status Check #####################
 
-    async def serverStatusCheck(self, ipaddr, port, passwod):
-  # Instigates connection to RCON and tries to get a playercount; if this fails the server is still starting.
+async def serverStatusCheck(self, cmd):
+# Instigates connection to RCON and tries to get a playercount; if this fails the server is still starting.
+    global serverStatus, ipaddr, port, passwod
+    serverProcessExists = await serverTest(self)
+    if serverProcessExists == True:
         try:
             with Client(ipaddr, port, passwd=passwod) as client:
-                playersOnline = client.run('listplayers')
-            self.serverStatus = 'Up'
+                # If statement that catches worldsave command without polluting serverStatus.
+                if cmd == 'saveworld':
+                    client.run(cmd)
+                    return
+                playersOnline = client.run(cmd)
+            serverStatus = 'Up'
             if 'No Players Connected' in playersOnline:
                 playerCount = 0
             else: 
                 playerCount = (playersOnline).count('\n')
         except:
         # If this is running and gives an error, the server is still going up. The player_count variable is dummied out for this instance.
-            self.serverStatus = 'Starting'
+            serverStatus = 'Starting'
             playerCount = -1
-        await Management.statusHeartbeat(self)
-        await Management.inactivityChecker(self, playerCount)
-        return(self.serverStatus)
+        await inactivityChecker(self, playerCount)
+    else:
+        serverStatus = 'Down'
+    return(serverStatus)
 
 #########################################################
 
 #################### Inactivity Check ###################
 
-    async def inactivityChecker(self, playersOnline):
-        if playersOnline == 0:
-            self.inactivityTime += 30
-        else:
-            self.inactivityTime = 0
-        if self.inactivityTime >= 7200:
-            print(str(datetime.datetime.now().time()) + ' - Server has been inactive for two hours! Shutting it down...')
-            await Management.terminateServer()
-            self.inactivityTime = 0
-        return
+async def inactivityChecker(self, playersOnline):
+    if playersOnline == 0:
+        self.inactivityTime += 30
+    else:
+        self.inactivityTime = 0
+    if self.inactivityTime >= 7200:
+        await logger(self, str(datetime.datetime.now().strftime("%H:%M:%S")) + 'Server has been inactive for two hours! Shutting it down...')
+        await terminateServer()
+        self.inactivityTime = 0
+    return
 
 #########################################################
 
 ################## After-Hours Shutdown #################
 
-    async def afterHoursShutdown(self, start_hour, end_hour):
-        if end_hour <= datetime.datetime.now().time() <= start_hour:
-            os.system("shutdown /s /t 1")
-        else:
-            print('After-Hours Shutdown: The time is ' + str(datetime.datetime.now().time()) + '. It\'s not even late yet!')
+async def afterHoursShutdown(self, start_hour, end_hour):
+    if end_hour <= datetime.datetime.now().time() <= start_hour:
+        os.system("shutdown /s /t 1")
+    else:
+        await logger(self, str(datetime.datetime.now().strftime("%H:%M:%S")) + 'After-Hours Shutdown: It\'s not even late yet!')
 
 #########################################################
 
 #################### Status Heartbeat ###################
 
-    async def statusHeartbeat(self):
-        if self.oldStatus != self.serverStatus:
-            print('Server state change: ' + self.oldStatus + ' -> ' + self.serverStatus)
-            self.oldStatus = self.serverStatus
-        if self.inactivityTime % 1800 == 0 and self.inactivityTime != 0:
-            print(str(datetime.datetime.now().strftime("%H:%M:%S")) + ' - ' + str(int(self.inactivityTime / 60)) + ' minutes have passed in inactivity.')
+async def statusHeartbeat(self):
+    if self.oldStatus != self.serverStatus:
+        await logger(self, 'Server state change: ' + self.oldStatus + ' -> ' + self.serverStatus)
+        self.oldStatus = self.serverStatus
+    if self.inactivityTime % 1800 == 0 and self.inactivityTime != 0 and self.serverStatus == 'Up':
+        await logger(self, str(int(self.inactivityTime / 60)) + ' minutes have passed in inactivity.')
+
+#########################################################
+
+####################### Logger ##########################
+
+async def logger(self, msg):
+    # Logic that determines what file is to be opened and written to. Logs change on the daily.
+    targetFile = str(datetime.date.today().year) + '-' + str(datetime.date.today().month) + '-' + str(datetime.date.today().day) + '-Gatekeeper-Log.txt'
+    targetPath = ".\\logs\\"
+    open(targetPath + targetFile, "a").writelines('[' + str(datetime.datetime.now().strftime("%H:%M:%S")) + '] - ' + msg + '\n')
+    open(targetPath + targetFile).close()
+    print('[' + str(datetime.datetime.now().strftime("%H:%M:%S")) + '] - ' + msg)
 
 #########################################################
